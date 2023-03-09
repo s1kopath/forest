bbeaba0b7ddeaa9479f884ab7284ca12adc35914
Akash0000439 <akashlog402@gmail.com>
2023-03-09 17:58:01 +0600

    add design referls section

--- resources/views/back-end/public/referrals/referrals.blade.php
+++ resources/views/back-end/public/referrals/referrals.blade.php
@@ -3,6 +3,39 @@
 
 @push('css')
     <link rel="stylesheet" type="text/css" href="{{ asset('back-end/assets/pages/treeview/treeview.css') }}">
+    @push('css')
+        <style>
+            .btn:hover {
+                background: linear-gradient(#152B40, #152B40);
+            }
+
+            .btn:focus {
+                background: linear-gradient(#152B40, #152B40);
+            }
+
+            .button-container button {
+                display: inline-block;
+                margin-right: 10px;
+                padding: 5px 10px;
+            }
+
+            @media only screen and (max-width: 768px) {
+                .shihab-btn-mbl-scroll {
+                    overflow-x: scroll;
+                    width: 100%;
+                    display: flex;
+                    position: relative;
+                }
+
+                .shihab-btn-mbl-scroll button {
+                    display: inline-block;
+                    position: relative;
+                    padding: 6px 15px !important;
+                    white-space: pre;
+                }
+            }
+        </style>
+    @endpush
 @endpush
 
 @section('page-title', 'Referrals')
@@ -56,6 +89,73 @@
                     <h3 class="font-weight-bold">Active</h3>
                 </div>
             </div>
+
+            <div class="card-block shihab-btn-mbl-scroll mt-4">
+                <button type="button" class="btn btn-success rounded-pill mx-1">Direct</button>
+                <button type="button" class="btn btn-success rounded-pill mx-1">IB</button>
+                <button type="button" class="btn btn-success rounded-pill mx-1">Pro-IB</button>
+                <button type="button" class="btn btn-success rounded-pill mx-1">Master-IB</button>
+                <button type="button" class="btn btn-success rounded-pill mx-1">Corporate-IB</button>
+            </div>
+            <div class="table-responsive mt-4">
+                <table class="table table-responsive">
+                    <thead>
+                        <tr>
+                            {{-- <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Username</th> --}}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>
+                                <h4 style="font-weight:bold">Danial001</h4>
+                                <h6>Diana Joshep Padex</h6>
+                            </td>
+                            <td>
+                                <h4 style="font-weight:bold">Register</h4>
+                                <h6>$10000</h6>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>
+                                <h4 style="font-weight:bold">Danial001</h4>
+                                <h6>Diana Joshep Padex</h6>
+                            </td>
+                            <td>
+                                <h4 style="font-weight:bold">IB</h4>
+                                <h6>$1000</h6>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>
+                                <h4 style="font-weight:bold">Danial001</h4>
+                                <h6>Diana Joshep Padex</h6>
+                            </td>
+                            <td>
+                                <h4 style="font-weight:bold">Master IB</h4>
+                                <h6>$1000</h6>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>
+                                <h4 style="font-weight:bold">Danial001</h4>
+                                <h6>Diana Joshep Padex</h6>
+                            </td>
+                            <td>
+                                <h4 style="font-weight:bold">Pro IB</h4>
+                                <h6>$1000</h6>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
             <div class="table-responsive mt-4">
                 <table class="table table-framed">
                     <thead>
@@ -91,6 +191,12 @@
             navigator.clipboard.writeText(copyText);
 
             $('#copied').html('✔️ Copied');
+        }
+    </script>
+    <script>
+        function focusMe(button) {
+            document.getElementsByClassName("button-selected")[0].className = "";
+            button.className = "button-selected";
         }
     </script>
 @endpush
