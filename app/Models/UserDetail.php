c1bfac0a73627b60166e01e95da8a9de4703c971
s1kopath <asaduzzaman.arob@gmail.com>
2023-03-11 17:05:22 +0600

    profile kyc update

--- app/Models/UserDetail.php
+++ app/Models/UserDetail.php
@@ -10,7 +10,7 @@ class UserDetail extends Model
     use HasFactory;
     protected $guarded = [];
 
-    public $appends = ['pic'];
+    public $appends = ['pic', 'kyc_pic_1', 'kyc_pic_2'];
 
     function getPicAttribute()
     {
@@ -23,6 +23,36 @@ class UserDetail extends Model
         }
 
         $storage_link = \Illuminate\Support\Facades\Storage::url($this->image);
+
+        return asset($storage_link);
+    }
+
+    function getKycPic1Attribute()
+    {
+        if ($this->kyc_image_1) {
+            if (!\Illuminate\Support\Facades\Storage::disk('public')->exists($this->kyc_image_1)) {
+                return asset('back-end/img/user.png');
+            }
+        } else {
+            return asset('back-end/img/user.png');
+        }
+
+        $storage_link = \Illuminate\Support\Facades\Storage::url($this->kyc_image_1);
+
+        return asset($storage_link);
+    }
+
+    function getKycPic2Attribute()
+    {
+        if ($this->kyc_image_2) {
+            if (!\Illuminate\Support\Facades\Storage::disk('public')->exists($this->kyc_image_2)) {
+                return asset('back-end/img/user.png');
+            }
+        } else {
+            return asset('back-end/img/user.png');
+        }
+
+        $storage_link = \Illuminate\Support\Facades\Storage::url($this->kyc_image_2);
 
         return asset($storage_link);
     }
