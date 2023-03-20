e1dc3d91e8e6493e141c2f5971cae07a352b4cf8
s1kopath <asaduzzaman.arob@gmail.com>
2023-03-20 19:04:43 +0600

    fix

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -19,36 +19,61 @@ class TestController extends Controller
 {
     public function test()
     {
+        // Generate nonce string
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $nonce = '';
+        for ($i = 1; $i <= 32; $i++) {
+            $pos = mt_rand(0, strlen($chars) - 1);
+            $char = $chars[$pos];
+            $nonce .= $char;
+        }
+        $ch = curl_init();
+        $timestamp = round(microtime(true) * 1000);
+        // Request body
+        $request = array(
+            "env" => array(
+                "terminalType" => "APP"
+            ),
+            "merchantTradeNo" => mt_rand(982538, 9825382937292),
+            "orderAmount" => 25.17,
+            "currency" => "BUSD",
+            "goods" => array(
+                "goodsType" => "01",
+                "goodsCategory" => "D000",
+                "referenceGoodsId" => "7876763A3B",
+                "goodsName" => "Ice Cream",
+                "goodsDetail" => "Greentea ice cream cone"
+            )
+        );
 
-        // return view('test');
-        $url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,tether&vs_currencies=eur,usd,gbp';
-        $data = file_get_contents($url);
-        $data = json_decode($data, true);
+        $json_request = json_encode($request);
+        $payload = $timestamp . "\n" . $nonce . "\n" . $json_request . "\n";
+        $binance_pay_key = "kzwhkpnVD8GF2GdvRfqcglg7BbvuRMuTbiFQb4lVxpdl9RxRUdC4Oq8tonSY8uWl";
+        $binance_pay_secret = "5mFZB6YY8J1ib92XdlrIxiq8p5xpODv3mpqk3cVzT87X3OuS8PHrvToxvOn15CQ1";
+        $signature = strtoupper(hash_hmac('SHA512', $payload, $binance_pay_secret));
+        $headers = array();
+        $headers[] = "Content-Type: application/json";
+        $headers[] = "BinancePay-Timestamp: $timestamp";
+        $headers[] = "BinancePay-Nonce: $nonce";
+        $headers[] = "BinancePay-Certificate-SN: $binance_pay_key";
+        $headers[] = "BinancePay-Signature: $signature";
 
-        $btc_usd = $data['bitcoin']['usd'];
-        $btc_eur = $data['bitcoin']['eur'];
-        $btc_gbp = $data['bitcoin']['gbp'];
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_URL, "https://bpay.binanceapi.com/binancepay/openapi/v2/order");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json_request);
 
-        $eth_usd = $data['ethereum']['usd'];
-        $eth_eur = $data['ethereum']['eur'];
-        $eth_gbp = $data['ethereum']['gbp'];
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
+        }
+        curl_close($ch);
 
-        $usdt_usd = $data['tether']['usd'];
-        $usdt_eur = $data['tether']['eur'];
-        $usdt_gbp = $data['tether']['gbp'];
+        var_dump($result);
 
-        echo "BTC USD: $btc_usd<br>";
-        echo "BTC EUR: $btc_eur<br>";
-        echo "BTC GBP: $btc_gbp<br>";
-
-        echo "ETH USD: $eth_usd<br>";
-        echo "ETH EUR: $eth_eur<br>";
-        echo "ETH GBP: $eth_gbp<br>";
-
-        echo "USDT USD: $usdt_usd<br>";
-        echo "USDT EUR: $usdt_eur<br>";
-        echo "USDT GBP: $usdt_gbp<br>";
-        dd('200');
-        abort(500);
+        //Redirect user to the payment page
+        // dd('200');
+        // abort(500);
     }
 }
