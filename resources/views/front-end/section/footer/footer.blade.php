4e78e6cff1b4709b5a2b4f14f2114682ab745073
s1kopath <asaduzzaman.arob@gmail.com>
2023-04-10 11:10:37 +0600

    footer changed

--- resources/views/front-end/section/footer/footer.blade.php
+++ resources/views/front-end/section/footer/footer.blade.php
@@ -2,69 +2,81 @@
     <div class="uk-section">
         <div class="uk-container uk-margin-top">
             <div class="uk-grid">
-                <div class="uk-width-2-3@m">
-                    <div class="uk-child-width-1-2@s uk-child-width-1-3@m" data-uk-grid="">
-                        <div>
-                            <h5>Instruments</h5>
-                            <ul class="uk-list uk-link-text">
-                                <li><a href="#">Stock</a></li>
-                                <li><a href="#">Indexes</a></li>
-                                <li><a href="#">Currencies</a></li>
-                                <li><a href="#">Metals<span
-                                            class="uk-label uk-margin-small-left in-label-small">Popular</span></a>
-                                </li>
-                                <li><a href="#">Oil and gas</a></li>
-                                <li><a href="#">Cryptocurrencies<span
-                                            class="uk-label uk-margin-small-left in-label-small">Popular</span></a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h5>Analytics</h5>
-                            <ul class="uk-list uk-link-text">
-                                <li><a href="#">World Markets</a></li>
-                                <li><a href="#">Trading Central<span
-                                            class="uk-label uk-margin-small-left in-label-small">New</span></a>
-                                </li>
-                                <li><a href="#">Forex charts online</a></li>
-                                <li><a href="#">Market calendar</a></li>
-                                <li><a href="#">Central banks<span
-                                            class="uk-label uk-margin-small-left in-label-small">New</span></a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="in-margin-top-60@s">
-                            <h5>Education</h5>
-                            <ul class="uk-list uk-link-text">
-                                <li><a href="#">Basic course</a></li>
-                                <li><a href="#">Introductory webinar</a></li>
-                                <li><a href="#">About academy</a></li>
-                            </ul>
-                        </div>
+
+
+                <div class="uk-width-1-3@m">
+                    <div class="">
+                        <img src="{{ asset('front-end/img/reeve-logo-2.png') }}" alt="logo">
                     </div>
-                </div>
-                <div class="uk-width-1-3@m uk-flex uk-flex-right@m">
+                    <style>
+                        .social-icons {
+                            padding: 14px;
+                        }
+
+                        .social-icons>a {
+                            padding: 10px;
+                        }
+                    </style>
                     <!-- social media begin -->
-                    <div class="uk-flex uk-flex-column social-media-list">
-                        <div><a href="https://www.facebook.com/indonez" class="color-facebook text-decoration-none"><i
-                                    class="fab fa-facebook-square"></i>
-                                Facebook</a></div>
-                        <div><a href="https://twitter.com/indonez_tw" class="color-twitter text-decoration-none"><i
-                                    class="fab fa-twitter"></i>
-                                Twitter</a></div>
-                        <div><a href="https://www.instagram.com/indonez_ig"
-                                class="color-instagram text-decoration-none"><i class="fab fa-instagram"></i>
-                                Instagram</a></div>
-                        <div><a href="#some-link" class="color-telegram text-decoration-none"><i
-                                    class="fab fa-telegram"></i> Telegram</a></div>
-                        <div><a href="#some-link" class="color-youtube text-decoration-none"><i
-                                    class="fab fa-youtube"></i> Youtube</a></div>
+                    <div class="uk-flex uk-flex-center social-icons">
+                        <a href="https://www.facebook.com/" class="text-decoration-none">
+                            <img src="{{ asset('front-end/img/icons/facebook-icon.png') }}" width="40px"
+                                alt="REEVE">
+                        </a>
+                        <a href="https://twitter.com/" class="text-decoration-none">
+                            <img src="{{ asset('front-end/img/icons/twitter-icon.png') }}" width="40px"
+                                alt="REEVE">
+                        </a>
+                        <a href="https://www.instagram.com/" class="text-decoration-none">
+                            <img src="{{ asset('front-end/img/icons/instagram-icon.png') }}" width="40px"
+                                alt="REEVE">
+                        </a>
+                        <a href="#some-link" class="text-decoration-none">
+                            <img src="{{ asset('front-end/img/icons/youtube-icon.png') }}" width="40px"
+                                alt="REEVE">
+                        </a>
                     </div>
                     <!-- social media end -->
                 </div>
+
+
+                <div class="uk-width-2-3@m">
+                    <div class="uk-child-width-1-2@s uk-child-width-1-3@m" data-uk-grid="">
+                        <div>
+                            <h5 style="color: #000"><b>Company</b></h5>
+                            <ul class="uk-list uk-link-text">
+                                <li><a href="#about">About Us</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                                <li><a href="#">Terms & Conditions</a></li>
+                                <li><a href="#">Return Policy</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h5 style="color: #000"><b>Popular Products</b></h5>
+                            <ul class="uk-list uk-link-text">
+                                <li><a href="{{ route('staking') }}">Staking</a></li>
+                                <li><a href="{{ route('forex_trading') }}">Forex Trading</li>
+                                <li><a href="{{ route('forex_broker') }}">Forex Broker</a></li>
+                                <li><a href="{{ route('e_commerce') }}">E-Commerce</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h5 style="color: #000"><b>Quick Links</b></h5>
+                            <ul class="uk-list uk-link-text">
+                                <li><a href="{{ route('public_login') }}">Sign In</a></li>
+                                <li><a href="{{ route('register') }}">Sign Up</a></li>
+                                <li><a href="#">News</a></li>
+                                <li><a href="{{ route('public_become_an_ib') }}">Become An IB</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
-        <hr class="uk-margin-large">
+
+
+        {{-- <hr class="uk-margin-large">
         <div class="uk-container">
             <div class="uk-grid uk-flex uk-flex-middle">
                 <div class="uk-width-2-3@m uk-text-small">
@@ -90,6 +102,7 @@
                     </span>
                 </div>
             </div>
-        </div>
+        </div> --}}
+
     </div>
 </footer>
